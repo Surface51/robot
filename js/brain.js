2222b75bf8d490c075b25727bd82eb5a6a74a1e8
Barry Abrams <barry@surface51.com>
2015-09-28 15:35:34 -0500

    Now only listens when space button down

--- js/brain.js
+++ js/brain.js
@@ -1,25 +1,50 @@
 
 
 var hello = function() {
-  $("#hello").slideDown("slow");
   $("#message-area").html("Hello!");
 };
 
-
+var allowed = true;
+var timeout;
 // When the page first loads.
 $( document ).ready(function() {
   if (annyang) {
     var commands = {
-      'hello':        hello,
+      'hello': hello
     };
 
     annyang.debug();
     annyang.addCommands(commands);
     annyang.setLanguage('en');
-    annyang.start();
+    //annyang.start();
   } else {
     
   }
+
+  $(window).keydown(function(e) {
+    if (e.keyCode === 0 || e.keyCode === 32) {
+      if (allowed) {
+        allowed = false;
+        console.log('Space down');
+        annyang.start();
+        clearTimeout(timeout);
+        $("body").css("background-color", "green");
+      }
+    }
+  });
+
+  $(window).keyup(function(e) {
+    if (e.keyCode === 0 || e.keyCode === 32) {
+      console.log('Space up');
+      $("body").css("background-color", "white");
+
+      timeout = setTimeout(function() {
+        allowed = true;
+        annyang.abort();
+    }, 1000);
+      
+    }
+  });
 });
 
 // When everything on the page has loaded.
